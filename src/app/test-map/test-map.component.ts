949ae5295b78da050a264bcc61d8a491c6719261
mosilogic <mosilogic@gmail.com>
2026-04-06 16:40:08 +0200

    second commit

--- src/app/test-map/test-map.component.ts
+++ src/app/test-map/test-map.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-mosi-logic',
+  selector: 'app-test-map',
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './test-map.component.html',
